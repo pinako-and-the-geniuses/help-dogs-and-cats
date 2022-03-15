9f6fc5e82026856ecf518f8e8a146f9efb1cfc99
김경석 <0xe82de@gmail.com>
2022-03-15 12:11:53 +0900

    #S06P22A302-133 Create: 커뮤니티 댓글 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/a302/domain/community/entity/CommunityComment.java
@@ -0,0 +1,56 @@
+package com.ssafy.a302.domain.community.entity;
+
+import com.ssafy.a302.domain.member.entity.Member;
+import com.ssafy.a302.global.entity.base.BaseLastModifiedEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.*;
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Table(
+        name = "tb_community_comment"
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(of = {"seq", "content", "isDeleted"})
+public class CommunityComment extends BaseLastModifiedEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_seq", columnDefinition = "BIGINT UNSIGNED")
+    private Long seq;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private boolean isDeleted;
+
+    @JoinColumn(name = "community_seq", nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Community community;
+
+    @JoinColumn(name = "member_seq", nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @JoinColumn(name = "parent_seq", nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private CommunityComment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = ALL)
+    private List<CommunityComment> children = new ArrayList<>();
+
+    @Builder
+    public CommunityComment(String content, Community community, Member member) {
+        this.content = content;
+        this.isDeleted = false;
+        this.community = community;
+        this.member = member;
+    }
+}
