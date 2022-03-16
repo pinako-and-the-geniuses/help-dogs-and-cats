18c524051b3afcf66ff90f0a6c8cc3b96903eb87
kjw <tirkffus@naver.com>
2022-03-16 15:11:29 +0900

    #S06P22A302-26 Feat 동물 상세조회수정

--- frontend/src/pages/animals/AnimalDetails.js
+++ frontend/src/pages/animals/AnimalDetails.js
@@ -1,39 +1,10 @@
-import { Card, CardImg, CardBody, Badge, CardSubtitle } from "reactstrap";
 import './styles/AnimalDetails.scss';
+import Map from 'components/Map';
 export default function AnimalDetails() {
   return (
     <div>
-      <h2 className="name">
-        <b>보호중 동물</b>
-      </h2>
-
-      <div>
-        <Card>
-          <CardImg
-            alt="Card image cap"
-            src="https://picsum.photos/256/186"
-            top
-            width="100%"
-          />
-          <CardBody>
-            
-              <Badge color="success" pill className="mb-3">
-                공고중
-              </Badge>
-            <CardSubtitle tag="h5">
-              <b>공고번호</b>
-            </CardSubtitle>
-            <br />
-            <CardSubtitle tag="h5">
-              <b>접수일시</b>
-            </CardSubtitle>
-            <br />
-            <CardSubtitle tag="h5">
-              <b>발견장소</b>
-            </CardSubtitle>
-            <br />
-          </CardBody>
-        </Card>
+      <div className="map">
+        <Map></Map>
       </div>
     </div>
   );
