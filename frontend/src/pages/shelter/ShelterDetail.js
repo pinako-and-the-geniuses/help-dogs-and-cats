5ee61379c090aa4c8f50cd32a4b04960c73b3a37
jiho <hahhaah@naver.com>
2022-03-17 00:07:48 +0900

    #S06P22A302-32 Style: 동물 보호 센터 목록 조회 컴포넌트

--- frontend/src/pages/shelter/ShelterDetail.js
+++ frontend/src/pages/shelter/ShelterDetail.js
@@ -4,13 +4,13 @@ import './styles/ShelterDetail.scss';
 function ShelterDetail(){
 
     return(
-        <div className='container'>
+        <div className='main-container'>
             <header>
                 <h2>동물 보호 센터</h2>
             </header>
 
             <main>
-                <section className="top-content">
+                {/* <section className="top-content">
                     <div className="main-info">
                         <div className="text-box">
                             <p className="title">보호센터명</p>
@@ -45,10 +45,45 @@ function ShelterDetail(){
                     <div className="map">
                         <Map></Map>
                     </div>
-                </section>
+                </section> */}
 
-                <section className="bottom-content">
-                    <p>음..더 이상은 굳이 필요 없을듯?</p>
+                <section className="container">
+                    <div className='row'>
+                        <div className=" col-6 main-info">
+                            <div className="text-box">
+                                <p className="title">보호센터명</p>
+                                <p>멀티캠퍼스</p>
+                            </div>
+                            <div className="text-box">
+                                <p className="title">전화번호</p>
+                                <p>02-000-0000</p>
+                            </div>
+                            <div className="text-box">
+                                <p className="title">주소</p>
+                                <p>서울특별시 강남구 테헤란로 121</p>
+                            </div>
+                            <div className="text-box">
+                                <p className="title">관리기관명</p>
+                                <p>삼성멀티캠퍼스 (법인)</p>
+                            </div>
+                            <div className="text-box">
+                                <p className="title">운영시간</p>
+                                <p>9:00 ~ 18:00</p>
+                            </div>
+                            <div className="text-box">
+                                <p className="title">휴무일</p>
+                                <p></p>
+                            </div>
+                            <div className="text-box">
+                                <p className="title">구조대상동물</p>
+                                <p>개+고양이+기타</p>
+                            </div>
+                        </div>
+
+                        <div className="col-6 map">
+                           <Map />
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
--- frontend/src/pages/shelter/ShelterList.js
+++ frontend/src/pages/shelter/ShelterList.js
@@ -4,15 +4,65 @@ import './styles/ShelterList.scss';
 function ShelterList(){
 
     return(
-        <div>
+        <div className='main-container'>
             <header>
                 <h2>동물 보호 센터</h2>
             </header>
 
-            <div>
+            <div className='my-city'>
                 <p>우리 지역 보호소 찾기</p>
             </div>
 
+            <div className='search-bar'>
+                <div className='search-input'>
+                    <p><label for="serachCd">시도</label></p>
+                    <select name='searchCd'>
+                        <option value="0">전체</option>
+                    </select>
+
+                    <p><label for="searchCgg">시군구</label></p>
+                    <select name='searchCgg'>
+                        <option value="0">전체</option>
+                    </select>
+
+                    <p>보호센터명</p>
+                    <input type="text" />
+                </div>
+
+                <button type='submit'>조회</button>
+            </div>
+
+            <table className="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                    {/* <th scope="col">#</th> */}
+                    <th scope="col">관할구역</th>
+                    <th scope="col">보호센터명</th>
+                    <th scope="col">전화번호</th>
+                    <th scope="col">보호센터 주소</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td scope="row">어디</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <td scope="row">어디</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <td scope="row">어디</td>
+                    <td>어쩌고저쩌고</td>
+                    <td>Larry the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
