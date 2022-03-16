9d74c01f1234be4e61008071bbb66981cb90ac58
jiho <hahhaah@naver.com>
2022-03-16 11:58:18 +0900

    #S06P22A302 Chore: 부트스트랩 수정:

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// styles - SCSS
-import "assets/scss/paper-kit.scss";
-// import "assets/demo/demo.css";
 
 ReactDOM.render(
   <React.StrictMode>
