4797e4d97e5534f73905b91d917e1b3935bc0a20
hyojeong721 <ase0574@naver.com>
2022-03-17 16:54:13 +0900

    #S06P22A302-87 Feat: 아이디 찾기 레이아웃 작성

--- frontend/src/pages/user/FindId.js
+++ frontend/src/pages/user/FindId.js
@@ -1,7 +1,37 @@
+import "./styles/userform.scss";
+
 export default function FindId() {
   return (
-    <div>
-      <h1>FindId page</h1>
+    <div className="userform-page">
+      <form className="find-form form">
+        <h2>아이디 찾기</h2>
+
+        <div className="row find-category">
+          <div className="col-4 now">
+            <a href="/user/findid">아이디찾기</a>
+            <hr />
+          </div>
+
+          <div className="col-4">
+            <a href="/user/findpwd">비밀번호 찾기</a>
+          </div>
+        </div>
+
+        <div className="find-id">
+          <p>가입 시 핸드폰 번호를 입력하세요.</p>
+
+          <input
+            type="email"
+            // onChange={onEmailHandler}
+            placeholder="핸드폰번호"
+          />
+        </div>
+
+        <button type="submit">아이디 찾기</button>
+        <p className="message">
+          아직 회원이 아니신가요? <a href="/signup">회원가입</a>
+        </p>
+      </form>
     </div>
   );
 }
