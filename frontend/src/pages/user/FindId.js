f16c1fb1e35f6cda7f098de7695227f41ff5fd56
hyojeong721 <ase0574@naver.com>
2022-03-18 11:08:16 +0900

    #S06P22A302-87 Feat: 핸드폰 입력 양식 수정

--- frontend/src/pages/user/FindId.js
+++ frontend/src/pages/user/FindId.js
@@ -18,7 +18,7 @@ export default function FindId() {
         </div>
 
         <div className="find-id">
-          <p>가입 시 핸드폰 번호를 입력하세요. ( '-' 제외)</p>
+          <p>가입 시 핸드폰 번호를 입력하세요.</p>
 
           <input
             type="tel"
