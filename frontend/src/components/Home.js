a4bc06604895da4caa9981fd54ae498efc29dcdf
jiho <hahhaah@naver.com>
2022-03-21 11:14:19 +0900

    #S06P22A302-136 Feat: 메인화면

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import style from './styles/Home.module.scss';
+
+function Home(){
+
+    return(
+        <div className={style.home_img}>
+            <h1>안녕하세요</h1>
+        </div>
+    )
+}
+
+export default Home;
