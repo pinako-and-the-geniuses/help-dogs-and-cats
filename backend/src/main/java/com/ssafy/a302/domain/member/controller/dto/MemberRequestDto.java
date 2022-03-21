2eea6e842d92de9cced54e4f1e0fee3a5f9b8bf1
김경석 <0xe82de@gmail.com>
2022-03-21 18:35:24 +0900

    #S06P22A302-63 Create: 회원 요청 DTO 생성

---
+++ backend/src/main/java/com/ssafy/a302/domain/member/controller/dto/MemberRequestDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.a302.domain.member.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Schema(name = "회원 요청 DTO", description = "회원 API 호출 시 사용되는 요청 DTO 입니다.")
+@Getter
+public class MemberRequestDto {
+
+}
