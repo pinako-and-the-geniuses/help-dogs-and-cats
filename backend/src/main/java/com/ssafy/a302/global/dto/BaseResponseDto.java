76bade8c9d06e7975e7b59436c73623b52fe050b
김경석 <0xe82de@gmail.com>
2022-03-21 18:27:38 +0900

    #S06P22A302-63 Refactor: 기본 응답 DTO 리팩토링
    - Object 타입을 제네릭 타입으로 수정하였습니다.

--- backend/src/main/java/com/ssafy/a302/global/dto/BaseResponseDto.java
+++ backend/src/main/java/com/ssafy/a302/global/dto/BaseResponseDto.java
@@ -1,17 +1,21 @@
 package com.ssafy.a302.global.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class BaseResponseDto {
-    
-    private String message;
+@Schema(name = "기본 응답 DTO", description = "API 호출이 성공하면 사용되는 DTO 입니다.")
+public class BaseResponseDto<T> {
 
-    private Object data;
+    @Schema(name = "message", title = "메시지", description = "결과 메시지입니다.", defaultValue = "null", nullable = true)
+    private final String message;
+
+    @Schema(name = "data", title = "데이터", description = "결과 데이터입니다.", defaultValue = "null", nullable = true)
+    private final T data;
 
     @Builder
-    public BaseResponseDto(String message, Object data) {
+    public BaseResponseDto(String message, T data) {
         this.message = message;
         this.data = data;
     }
