4aeebf1f2bb1dc90e7904d6d99c0975b82ce8fcb
김경석 <0xe82de@gmail.com>
2022-03-21 18:28:18 +0900

    #S06P22A302-63 Create: 중복 예외 클래스 추가

---
+++ backend/src/main/java/com/ssafy/a302/global/exception/DuplicateException.java
@@ -0,0 +1,8 @@
+package com.ssafy.a302.global.exception;
+
+public class DuplicateException extends RuntimeException {
+
+    public DuplicateException(String message) {
+        super(message);
+    }
+}
