83eb7d548227277ffb46cefb386636dd81886369
김경석 <0xe82de@gmail.com>
2022-03-21 18:39:40 +0900

    #S06P22A302-63 Feat: 회원가입 API 추가

--- backend/src/main/java/com/ssafy/a302/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/a302/domain/member/controller/MemberController.java
@@ -1,34 +1,61 @@
 package com.ssafy.a302.domain.member.controller;
 
+import com.ssafy.a302.domain.member.controller.dto.MemberRequestDto;
 import com.ssafy.a302.domain.member.service.MemberService;
 import com.ssafy.a302.global.dto.BaseResponseDto;
+import com.ssafy.a302.global.dto.ErrorResponseDto;
+import com.ssafy.a302.global.message.Message;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
 @RestController
+@Tag(name = "회원", description = "회원 데이터 관련 REST API가 작성됩니다.")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/test/{email}")
-    public BaseResponseDto test(@PathVariable(name = "email") String email) {
-
-        String message = "";
-        Object data = null;
-
-        message = "API 호출 성공";
-        data = memberService.findMemberByEmail(email).getEmail();
+    @Operation(
+            summary = "회원가입 API",
+            description = "이메일, 패스워드, 닉네임, 핸드폰 번호, 활동 지역을 전달받고 회원가입을 진행합니다.",
+            tags = {"member"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "회원가입에 성공하였습니다.",
+                    content = @Content(schema = @Schema(implementation = BaseResponseDto.class))),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "이메일, 패스워드, 닉네임, 핸드폰 번호 중 한 가지 이상 형식 검증에 실패하였거나, 패스워드에 이메일 또는 닉네임이 포함됩니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "이메일 또는 닉네임이 중복됩니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "서버에 문제가 발생하였습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public BaseResponseDto<?> register(@Validated @RequestBody MemberRequestDto.RegisterInfo registerInfo) {
+        memberService.register(registerInfo.toServiceDto());
 
         return BaseResponseDto.builder()
-                .message(message)
-                .data(data)
+                .message(Message.REGISTER_MEMBER)
                 .build();
     }
 }
