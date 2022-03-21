3634dc975181b7ee9eb015e99eed48f1af3429fe
김경석 <0xe82de@gmail.com>
2022-03-21 18:31:14 +0900

    #S06P22A302-63 Feat: 글로벌 예외 핸들링 메서드 추가
    - MethodArgumentNotValidException.class : Validation 검증 실패 시 발생되는 예외 클래스
    - DuplicationException.class : 중복 데이터 발생 시 발생되는 예외 클래스
    - Exception.class : 기타 예외 발생 시 핸들링

--- backend/src/main/java/com/ssafy/a302/global/exception/handler/GlobalControllerAdvice.java
+++ backend/src/main/java/com/ssafy/a302/global/exception/handler/GlobalControllerAdvice.java
@@ -1,11 +1,20 @@
 package com.ssafy.a302.global.exception.handler;
 
-import com.ssafy.a302.global.dto.BaseResponseDto;
+import com.ssafy.a302.global.dto.ErrorResponseDto;
+import com.ssafy.a302.global.exception.DuplicateException;
+import com.ssafy.a302.global.message.ErrorMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -13,11 +22,60 @@ public class GlobalControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public BaseResponseDto illegalArgumentExceptionHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] ex", e);
-        return BaseResponseDto.builder()
+    public ErrorResponseDto<?> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        logPrint(e);
+        return ErrorResponseDto.builder()
                 .message(e.getMessage())
-                .data(null)
                 .build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponseDto<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        logPrint(e);
+
+        Map<String, Object> data = new HashMap<>();
+
+        BindingResult bindingResult = e.getBindingResult();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            Map<String, String> errorInfo = new HashMap<>();
+            errorInfo.put("field", fieldError.getField());
+            errorInfo.put("message", fieldError.getDefaultMessage());
+            data.put("fieldError", errorInfo);
+        }
+
+        for (ObjectError globalError : bindingResult.getGlobalErrors()) {
+            Map<String, String> errorInfo = new HashMap<>();
+            errorInfo.put("field", globalError.getObjectName());
+            errorInfo.put("message", globalError.getDefaultMessage());
+            data.put("globalError", errorInfo);
+        }
+
+        return ErrorResponseDto.builder()
+                .message(ErrorMessage.PATTERN)
+                .data(data)
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DuplicateException.class)
+    public ErrorResponseDto<?> duplicationExceptionHandler(DuplicateException e) {
+        logPrint(e);
+        return ErrorResponseDto.builder()
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorResponseDto<?> exceptionHandler(Exception e) {
+        logPrint(e);
+        return ErrorResponseDto.builder()
+                .message(ErrorMessage.ERROR)
+                .build();
+    }
+
+    private void logPrint(Exception e) {
+        log.error("[exceptionHandler] ex", e);
     }
 }
