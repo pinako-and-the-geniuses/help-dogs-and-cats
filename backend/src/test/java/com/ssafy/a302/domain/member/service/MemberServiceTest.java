bc06569338544c9b78d6a4ac17f1822e6cc63889
김경석 <0xe82de@gmail.com>
2022-03-21 18:54:53 +0900

    #S06P22A302-63 Test: 회원가입 - 성공 테스트 메서드 추가

--- backend/src/test/java/com/ssafy/a302/domain/member/service/MemberServiceTest.java
+++ backend/src/test/java/com/ssafy/a302/domain/member/service/MemberServiceTest.java
@@ -1,12 +1,18 @@
 package com.ssafy.a302.domain.member.service;
 
 import com.ssafy.a302.domain.member.controller.dto.MemberRequestDto;
+import com.ssafy.a302.domain.member.entity.Member;
 import com.ssafy.a302.domain.member.repository.MemberRepository;
+import com.ssafy.a302.domain.member.service.dto.MemberDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -42,5 +48,25 @@ class MemberServiceTest {
                 .tel("010-0000-0002")
                 .activityArea("서울시 도봉구")
                 .build();
+    }
+
+    @Test
+    @DisplayName("회원가입 - 성공")
+    void registerSuccess() {
+        /**
+         * registerInfo1 회원가입
+         */
+        MemberDto.Response responseDto = memberService.register(registerInfo1.toServiceDto());
+
+        /**
+         * 데이터 검증
+         */
+        assertThat(responseDto.getSeq()).isNotNull();
+        assertThat(responseDto.getEmail()).isEqualTo(registerInfo1.getEmail());
+        assertThat(passwordEncoder.matches(registerInfo1.getPassword(), responseDto.getPassword())).isTrue();
+        assertThat(responseDto.getNickname()).isEqualTo(registerInfo1.getNickname());
+        assertThat(responseDto.getTel()).isEqualTo(registerInfo1.getTel());
+        assertThat(responseDto.getActivityArea()).isEqualTo(registerInfo1.getActivityArea());
+        assertThat(responseDto.getRole()).isEqualTo(Member.Role.MEMBER);
     }
 }
