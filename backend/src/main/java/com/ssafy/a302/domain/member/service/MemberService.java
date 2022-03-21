fcc47559a4f584425cb423872b25e58c570e1fd6
김경석 <0xe82de@gmail.com>
2022-03-21 18:48:46 +0900

    #S06P22A302-63 Feat: 회원가입 서비스 메서드 추가

--- backend/src/main/java/com/ssafy/a302/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/a302/domain/member/service/MemberService.java
@@ -1,8 +1,11 @@
 package com.ssafy.a302.domain.member.service;
 
 import com.ssafy.a302.domain.member.entity.Member;
+import com.ssafy.a302.domain.member.service.dto.MemberDto;
 
 public interface MemberService {
 
     Member findMemberByEmail(String email);
+
+    MemberDto.Response register(MemberDto memberDto);
 }
--- backend/src/main/java/com/ssafy/a302/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/ssafy/a302/domain/member/service/MemberServiceImpl.java
@@ -1,10 +1,18 @@
 package com.ssafy.a302.domain.member.service;
 
 import com.ssafy.a302.domain.member.entity.Member;
+import com.ssafy.a302.domain.member.entity.MemberDetail;
+import com.ssafy.a302.domain.member.exception.DuplicateEmailException;
+import com.ssafy.a302.domain.member.exception.DuplicateNicknameException;
+import com.ssafy.a302.domain.member.repository.MemberDetailRepository;
 import com.ssafy.a302.domain.member.repository.MemberRepository;
+import com.ssafy.a302.domain.member.service.dto.MemberDto;
+import com.ssafy.a302.global.message.ErrorMessage;
+import com.ssafy.a302.global.message.Message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +24,38 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final MemberDetailRepository memberDetailRepository;
+
     private final MessageSource messageSource;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Member findMemberByEmail(String email) {
         return memberRepository.findMemberByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException(messageSource.getMessage("bad.member.email", null , null)));
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.NULL_MEMBER);
+    }
+
+    @Transactional
+    @Override
+    public MemberDto.Response register(MemberDto memberDto) {
+        if (memberRepository.existsByEmail(memberDto.getEmail())) {
+            throw new DuplicateEmailException(Message.DUPLICATE_MEMBER_EMAIL);
+        } else if (memberDetailRepository.existsByNickname(memberDto.getNickname())) {
+            throw new DuplicateNicknameException(Message.DUPLICATE_MEMBER_NICKNAME);
+        } else if (memberDto.getPassword().contains(memberDto.getEmail().split("@")[0])) {
+            throw new IllegalArgumentException(Message.PASSWORD_CONTAIN_MEMBER_EMAIL);
+        } else if (memberDto.getPassword().contains(memberDto.getNickname())) {
+            throw new IllegalArgumentException(Message.PASSWORD_CONTAIN_MEMBER_NICKNAME);
+        }
+
+        Member member = memberDto.toEntity();
+        member.changeRole(Member.Role.MEMBER);
+        member.encryptPassword(passwordEncoder);
+        MemberDetail detail = memberDto.toDetailEntity(member);
+
+        Member savedMember = memberRepository.save(member);
+
+        return savedMember.toResponseDto();
     }
 }
