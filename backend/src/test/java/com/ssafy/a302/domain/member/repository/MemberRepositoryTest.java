4e0df0b376211004e7d2f60c5e831506531a5b58
김경석 <0xe82de@gmail.com>
2022-03-22 23:38:44 +0900

    #S06P22A302-61 Test: 기본키로 회원 엔티티 조회 테스트 메서드 추가

--- backend/src/test/java/com/ssafy/a302/domain/member/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/ssafy/a302/domain/member/repository/MemberRepositoryTest.java
@@ -2,12 +2,17 @@ package com.ssafy.a302.domain.member.repository;
 
 import com.ssafy.a302.domain.member.entity.Member;
 import com.ssafy.a302.domain.member.entity.MemberDetail;
+import com.ssafy.a302.global.message.ErrorMessage;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -39,5 +44,40 @@ class MemberRepositoryTest {
                 .tel("010-0001-0001")
                 .activityArea("서울시 강남구")
                 .build();
+    }
+
+    @Test
+    @DisplayName("기본키로 회원 엔티티 조회")
+    void findMemberBySeq() {
+        Member savedMember = memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        /**
+         * 기본키로 엔티티 조회
+         */
+        Member findMember = memberRepository.findMemberBySeq(savedMember.getSeq())
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_MEMBER_SEQ));
+
+        /**
+         * 동등성 비교
+         */
+        assertThat(findMember)
+                .usingRecursiveComparison()
+                .ignoringFields("detail.member.createdDate", "detail.member.lastModifiedDate", "createdDate", "lastModifiedDate")
+                .isEqualTo(member1);
+
+        /**
+         * 삭제 처리
+         */
+        findMember.delete();
+
+        /**
+         * 삭제 처리 후에 null이 반환되는지 확인한다.
+         */
+        assertThatThrownBy(() -> memberRepository.findMemberBySeq(savedMember.getSeq())
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_MEMBER_SEQ)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_MEMBER_SEQ);
     }
 }
