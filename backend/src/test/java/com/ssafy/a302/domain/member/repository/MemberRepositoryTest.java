e874dfc22a231a8c50ea332251610bb057e78355
김경석 <0xe82de@gmail.com>
2022-03-22 23:38:00 +0900

    #S06P22A302-61 Feat: setUp 데이터 추가

--- backend/src/test/java/com/ssafy/a302/domain/member/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/ssafy/a302/domain/member/repository/MemberRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.ssafy.a302.domain.member.repository;
 
+import com.ssafy.a302.domain.member.entity.Member;
+import com.ssafy.a302.domain.member.entity.MemberDetail;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,4 +18,26 @@ class MemberRepositoryTest {
 
     @Autowired
     private EntityManager em;
+
+    private Member member1;
+
+    private MemberDetail detail1;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.deleteAll();
+
+        member1 = Member.builder()
+                .email("test1@test.com")
+                .password("pass12#$")
+                .role(Member.Role.MEMBER)
+                .build();
+
+        detail1 = MemberDetail.builder()
+                .member(member1)
+                .nickname("good1")
+                .tel("010-0001-0001")
+                .activityArea("서울시 강남구")
+                .build();
+    }
 }
