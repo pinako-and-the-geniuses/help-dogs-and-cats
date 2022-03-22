ead1a0e6a941a36b95552c11487981185df12664
pinako <fufru@naver.com>
2022-03-22 15:30:52 +0900

    #S06P22A302-80 Feat: 스케쥴러 설정 및 추가

--- backend/src/main/java/com/ssafy/a302/A302Application.java
+++ backend/src/main/java/com/ssafy/a302/A302Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class A302Application {
 
