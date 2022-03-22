1ca6df68d070afcec3699126f5fdf6e6db390fd4
김경석 <0xe82de@gmail.com>
2022-03-22 16:03:45 +0000

    Feat: JPA Auditing

--- backend/src/main/java/com/ssafy/a302/A302Application.java
+++ backend/src/main/java/com/ssafy/a302/A302Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class A302Application {
 
