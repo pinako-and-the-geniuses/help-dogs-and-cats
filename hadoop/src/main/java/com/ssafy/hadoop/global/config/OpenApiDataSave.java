c8214553e4de71e37e49a0f1b0f9c6042f266f3f
안영진 <fufru@naver.com>
2022-03-24 01:51:47 +0900

    #S06P22A302-119 Feat: API 데이터 호출 코드 작성
    올해 포함 최신 2년의 데이터 호출,
    2년전의 데이터 데이터 호출 코드 작성

--- hadoop/src/main/java/com/ssafy/hadoop/global/config/OpenApiDataSave.java
+++ hadoop/src/main/java/com/ssafy/hadoop/global/config/OpenApiDataSave.java
@@ -1,18 +1,13 @@
 package com.ssafy.hadoop.global.config;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.time.LocalDate;
 
 @Component
 public class OpenApiDataSave {
@@ -20,99 +15,41 @@ public class OpenApiDataSave {
     @Value("${openapi.serviceKey}")
     private String serviceKey;
 
-    @Scheduled(cron = "0 0/1 * * * *")
-    public void getAnimalData() throws IOException, ParseException {
-        // 1. URL을 만들기 위한 StringBuilder.
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic");
+    //30분 마다 최근 2년치 데이터 받아와서 db에 저장해줘야함
+    @Async
+    @Scheduled(cron = "0 0/30 * * * *")
+    public void getRecentAnimalData() throws IOException, ParseException {
 
+        LocalDate curDate = LocalDate.now();
 
+        GetLastYearData glyd = new GetLastYearData();
 
-        // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
-        urlBuilder.append("?" + URLEncoder.encode("bgnde", "UTF-8") + "=" + URLEncoder.encode("20200101", "UTF-8")); //시작 일자
-        urlBuilder.append("&" + URLEncoder.encode("endde", "UTF-8") + "=" + URLEncoder.encode("20201231", "UTF-8")); //종료 일자
-        urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); //페이지 번호
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); //줄 수
-        urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*XML 또는 JSON*/
+        glyd.getLastYearData(curDate.getYear(),"recent",false);
 
-        // 3. URL 객체 생성.
-        URL url = new URL(urlBuilder.toString());
+        GetThisYearData gtyd = new GetThisYearData();
 
-        // 4. 요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성.
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        gtyd.getThisYearData(curDate);
 
-        // 5. 통신을 위한 메소드 SET.
-        conn.setRequestMethod("GET");
+        //sh코드 실행
 
-        // 6. 통신을 위한 Content-type SET.
-        conn.setRequestProperty("Content-type", "application/json");
+        //db저장
 
-        // 7. 통신 응답 코드 확인.
-        System.out.println("Response code: " + conn.getResponseCode());
+    }
 
-        // 8. 전달받은 데이터를 BufferedReader 객체로 저장.
-        BufferedReader rd;
-        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
+    //매년 초가 되면 2년전의 데이터를 영구 저장해줌
+    @Async
+    @Scheduled(cron = "0 10 0 1 1 ?")
+    public void saveLastTwoYearData() throws IOException, ParseException {
 
-        // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장.
-        StringBuilder sb = new StringBuilder();
+        LocalDate curDate = LocalDate.now();
 
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
+        GetLastYearData glyd = new GetLastYearData();
 
-        // 10. 객체 해제.
-        rd.close();
-        conn.disconnect();
+        glyd.getLastYearData(curDate.getYear()-2,"lastTwoYearData", false);
 
-        // 11. 전달받은 데이터 확인.
-//        System.out.println(sb.toString());
-        JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(sb.toString());
-        JSONArray jsonArray = (JSONArray) ((JSONObject) ((JSONObject) ((JSONObject) jsonObject.get("response")).get("body")).get("items")).get("item");
+        //sh코드 실행
 
-//        while(jj.hasNext()){
-//            System.out.println(jj.next().toString());
-//        }
-        //sexCd 성별
-        //kindCd 품종
-        //colorCd 색
-        //age 나이
-        //weight 무게
-        //processState 현재 상태
-        //neuterYn 중성화 여부 Y-함 N-안함 U-미상
-        //specialMark 특징 질병 상태 등등
-        //orgNm 지역
-        //happenDt 접수일
-
-        //https://www.hadoopinrealworld.com/writing-a-file-to-hdfs-java-program/
-//        https://docs.microsoft.com/ko-kr/azure/hdinsight/hadoop/apache-hadoop-develop-deploy-java-mapreduce-linux
-        StringBuilder ans = new StringBuilder();
-        for (Object o : jsonArray) {
-            JSONObject obj = (JSONObject) o;
-            ans.append(obj.get("kindCd").toString().trim()).append(",")
-                    .append(obj.get("neuterYn").toString().trim()).append(",")
-                    .append(obj.get("processState").toString().trim()).append(",")
-                    .append(obj.get("age").toString().trim()).append(",")
-                    .append(obj.get("happenDt").toString().trim()).append(",")
-                    .append(obj.get("orgNm").toString().trim())
-                    .append("\n");
-        }
-
-        FileOutputStream fos = new FileOutputStream("C:\\data\\breed-age.txt", false);
-        OutputStreamWriter writer = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
-        BufferedWriter out = new BufferedWriter(writer);
-
-        out.write(ans.toString());
-
-        out.flush();
-        out.close();
-
+        //db저장
     }
 
 
