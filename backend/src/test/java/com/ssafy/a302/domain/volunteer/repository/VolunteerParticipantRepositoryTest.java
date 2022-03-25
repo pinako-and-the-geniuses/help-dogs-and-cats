2b286468d616e68f47354b73b89aab633f5a9b1e
limgeonho <ghlim909@gmail.com>
2022-03-25 16:17:45 +0900

    #S06P22A302-65 Feat: 봉사활동 등록관련 테스트 케이스 작성

---
+++ backend/src/test/java/com/ssafy/a302/domain/volunteer/repository/VolunteerParticipantRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.ssafy.a302.domain.volunteer.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VolunteerParticipantRepositoryTest {
+
+    @Test
+    void findByMemberSeqAndVolunteerSeq() {
+    }
+
+    @Test
+    void findVolunteerParticipantBySeq() {
+    }
+}
