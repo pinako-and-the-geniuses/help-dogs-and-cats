c126bd63fb6c167e995198530511613d360f5818
김경석 <0xe82de@gmail.com>
2022-03-27 02:03:11 +0900

    #S06P22A302-148 Feat: 커뮤니티 등록 서비스 메서드 추가

--- backend/src/main/java/com/ssafy/a302/domain/community/service/CommunityService.java
+++ backend/src/main/java/com/ssafy/a302/domain/community/service/CommunityService.java
@@ -1,7 +1,8 @@
 package com.ssafy.a302.domain.community.service;
 
+import com.ssafy.a302.domain.community.service.dto.CommunityDto;
+
 public interface CommunityService {
 
-
-
+    Long register(CommunityDto communityDto, Long memberSeq);
 }
--- backend/src/main/java/com/ssafy/a302/domain/community/service/CommunityServiceImpl.java
+++ backend/src/main/java/com/ssafy/a302/domain/community/service/CommunityServiceImpl.java
@@ -1,6 +1,11 @@
 package com.ssafy.a302.domain.community.service;
 
+import com.ssafy.a302.domain.community.entity.Community;
 import com.ssafy.a302.domain.community.repository.CommunityRepository;
+import com.ssafy.a302.domain.community.service.dto.CommunityDto;
+import com.ssafy.a302.domain.member.entity.Member;
+import com.ssafy.a302.domain.member.repository.MemberRepository;
+import com.ssafy.a302.global.constant.ErrorMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,4 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommunityServiceImpl implements CommunityService {
 
     private final CommunityRepository communityRepository;
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Long register(CommunityDto communityDto, Long memberSeq) {
+        Member writer = memberRepository.findMemberBySeq(memberSeq)
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_MEMBER_SEQ));
+
+        Community newCommunity = communityDto.toEntity(writer);
+        return communityRepository.save(newCommunity).getSeq();
+    }
 }
