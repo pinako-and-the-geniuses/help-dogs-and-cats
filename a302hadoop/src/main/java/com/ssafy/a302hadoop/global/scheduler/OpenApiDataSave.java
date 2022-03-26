62fc39aa3cc2e86cf17553ed6d0c3bc228e18a1e
안영진 <fufru@naver.com>
2022-03-26 17:08:28 +0900

    #S06P22A302-186 Feat: 하둡 데이터 DB Insert
    스케쥴러 시간 변경 및 하둡 서비스 객체 주입

--- a302hadoop/src/main/java/com/ssafy/a302hadoop/global/scheduler/OpenApiDataSave.java
+++ a302hadoop/src/main/java/com/ssafy/a302hadoop/global/scheduler/OpenApiDataSave.java
@@ -1,6 +1,10 @@
 package com.ssafy.a302hadoop.global.scheduler;
 
+import com.ssafy.a302hadoop.global.config.GetLastYearData;
+import com.ssafy.a302hadoop.global.config.GetThisYearData;
+import com.ssafy.a302hadoop.global.config.SaveAnimalDatas;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,11 +15,14 @@ import java.time.LocalDate;
 @Component
 public class OpenApiDataSave {
 
+    @Autowired
+    SaveAnimalDatas saveAnimalDatas;
+
     @Value("${openapi.serviceKey}")
     private String serviceKey;
 
     //30분 마다 최근 2년치 데이터 받아와서 db에 저장해줘야함
-    @Scheduled(cron = "0 0/30 * * * *")
+    @Scheduled(cron = "0 10,40 * * * *")
     public void getRecentAnimalData() throws IOException, ParseException {
 
         LocalDate curDate = LocalDate.now();
@@ -29,25 +36,31 @@ public class OpenApiDataSave {
         gtyd.getThisYearData(curDate, serviceKey);
 
         //sh코드 실행
+        // https://codechacha.com/ko/java-run-shell-script/ 참고하자
 
         //db저장
 
+
+        saveAnimalDatas.saveData(LocalDate.now().getYear());
+
+
+
     }
 
-    //매년 초가 되면 2년전의 데이터를 영구 저장해줌
-    @Scheduled(cron = "0 10 0 1 1 ?")
-    public void saveLastTwoYearData() throws IOException, ParseException {
-
-        LocalDate curDate = LocalDate.now();
-
-        GetLastYearData glyd = new GetLastYearData();
-
-        glyd.getLastYearData(curDate.getYear() - 2, "lastTwoYearData", false, serviceKey);
-
-        //sh코드 실행
-
-        //db저장
-    }
+//    //매년 초가 되면 2년전의 데이터를 영구 저장해줌
+//    @Scheduled(cron = "0 10 0 1 1 ?")
+//    public void saveLastTwoYearData() throws IOException, ParseException {
+//
+//        LocalDate curDate = LocalDate.now();
+//
+//        GetLastYearData glyd = new GetLastYearData();
+//
+//        glyd.getLastYearData(curDate.getYear() - 2, "lastTwoYearData", false, serviceKey);
+//
+//        //sh코드 실행
+//
+//        //db저장
+//    }
 
 
 }
