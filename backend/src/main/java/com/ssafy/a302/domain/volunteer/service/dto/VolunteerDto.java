0a2cd5c97ea278999d466a4e9daffbee847d7cba
limgeonho <ghlim909@gmail.com>
2022-03-27 23:29:40 +0900

    #S06P22A302-49 Feat: 봉사활동 상세 페이지 조회

--- backend/src/main/java/com/ssafy/a302/domain/volunteer/service/dto/VolunteerDto.java
+++ backend/src/main/java/com/ssafy/a302/domain/volunteer/service/dto/VolunteerDto.java
@@ -1,11 +1,14 @@
 package com.ssafy.a302.domain.volunteer.service.dto;
 
-import com.ssafy.a302.domain.member.entity.Member;
 import com.ssafy.a302.domain.volunteer.entity.Volunteer;
+import com.ssafy.a302.domain.volunteer.entity.VolunteerComment;
+import com.ssafy.a302.domain.volunteer.service.VolunteerServiceImpl;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+
+import java.util.List;
 
 @Getter
 public class VolunteerDto {
@@ -82,6 +85,39 @@ public class VolunteerDto {
             this.activityArea = activityArea;
             this.minParticipantCount = minParticipantCount;
             this.maxParticipantCount = maxParticipantCount;
+        }
+    }
+
+    @Getter
+    @ToString(of = {"memberSeq", "nickname", "title", "content", "status", "activityArea", "volunteerComment"})
+    public static class DetailResponse {
+
+        private final Long memberSeq;
+
+        private final String nickname;
+
+        private final String title;
+
+        private final String content;
+
+        private final Volunteer.Status status;
+
+        private final String activityArea;
+
+        private List<VolunteerServiceImpl.SimpleVolunteerCommentDto> volunteerComment;
+
+        public void setVolunteerComment(List<VolunteerServiceImpl.SimpleVolunteerCommentDto> volunteerComment){
+            this.volunteerComment = volunteerComment;
+        }
+
+        @Builder
+        public DetailResponse(Long memberSeq, String nickname, String title, String content, Volunteer.Status status, String activityArea, List<VolunteerComment> volunteerComment) {
+            this.memberSeq = memberSeq;
+            this.nickname = nickname;
+            this.title = title;
+            this.content = content;
+            this.status = status;
+            this.activityArea = activityArea;
         }
     }
 
