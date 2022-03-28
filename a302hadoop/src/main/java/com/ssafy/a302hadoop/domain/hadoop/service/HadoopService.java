4388e84e687c9672a9137506f116634158ef2256
안영진 <fufru@naver.com>
2022-03-29 03:37:38 +0900

    #S06P22A302-187 setting: Fix: 유기동물 데이터 저장 기능 수정
    전체 데이터 저장 기능도 수행하도록 확장함

--- a302hadoop/src/main/java/com/ssafy/a302hadoop/domain/hadoop/service/HadoopService.java
+++ a302hadoop/src/main/java/com/ssafy/a302hadoop/domain/hadoop/service/HadoopService.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface HadoopService {
 
-    void insertAnimalData(List<DataFromHadoopDto> datasFromHadoopDto, int year);
+    void insertAnimalData(List<DataFromHadoopDto> datasFromHadoopDto, int year, String name);
 }
