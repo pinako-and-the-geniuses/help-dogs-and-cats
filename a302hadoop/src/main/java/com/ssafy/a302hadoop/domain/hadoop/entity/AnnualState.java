99b1a80045703036ab5c4576a573b50f40c56bf3
안영진 <fufru@naver.com>
2022-03-29 03:24:14 +0900

    #S06P22A302-187 Create: 연간 유기동물 처리상태 현황 Entity 추가

---
+++ a302hadoop/src/main/java/com/ssafy/a302hadoop/domain/hadoop/entity/AnnualState.java
@@ -0,0 +1,44 @@
+package com.ssafy.a302hadoop.domain.hadoop.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+@Entity
+@Table(
+        name = "tb_annual_state_list"
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(of = {"seq", "species", "state", "year", "count"})
+public class AnnualState {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "data_seq", columnDefinition = "BIGINT UNSIGNED")
+    private Long seq;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AnimalData.ProcessState state;
+
+    @Column(nullable = false)
+    private String species;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Builder
+    public AnnualState(String species, AnimalData.ProcessState state, int year, int count) {
+        this.species = species;
+        this.state = state;
+        this.year = year;
+        this.count = count;
+    }
+}
