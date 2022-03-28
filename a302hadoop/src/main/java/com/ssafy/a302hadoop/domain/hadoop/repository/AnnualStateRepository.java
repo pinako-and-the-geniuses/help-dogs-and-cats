e49ee09659d1326ad6c47332d08c5ddbe077b712
안영진 <fufru@naver.com>
2022-03-29 03:25:10 +0900

    #S06P22A302-187 Create: 연간 유기동물 처리상태 현황 Repository 추가

---
+++ a302hadoop/src/main/java/com/ssafy/a302hadoop/domain/hadoop/repository/AnnualStateRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.a302hadoop.domain.hadoop.repository;
+
+import com.ssafy.a302hadoop.domain.hadoop.entity.AnnualState;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnualStateRepository extends JpaRepository<AnnualState, Long> {
+
+}
+
+
