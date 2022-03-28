ff90a27faf154f6e4726d07b2729f5006aeddcff
김경석 <0xe82de@gmail.com>
2022-03-28 22:27:32 +0900

    #S06P22A302-163 Create: 입양 인증 수정 요청 DTO 생성

--- backend/src/main/java/com/ssafy/a302/domain/adopt/controller/dto/AdoptAuthRequestDto.java
+++ backend/src/main/java/com/ssafy/a302/domain/adopt/controller/dto/AdoptAuthRequestDto.java
@@ -36,4 +36,30 @@ public class AdoptAuthRequestDto {
         }
     }
 
+    @Getter
+    @NoArgsConstructor
+    public static class ModifyAdoptAuthInfo {
+
+        @Schema(name = "title", title = "입양 인증 제목", description = "입양 인증 제목입니다.")
+        @NotBlank(message = "${pattern.blank}")
+        private String title;
+
+        @Schema(name = "content", title = "입양 인증 본문", description = "입양 인증 본문입니다.")
+        @NotBlank(message = "${pattern.blank}")
+        private String content;
+
+        @Builder
+        public ModifyAdoptAuthInfo(String title, String content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        public AdoptDto.AdoptAuth toServiceDto() {
+            return AdoptDto.AdoptAuth.builder()
+                    .title(title)
+                    .content(content)
+                    .build();
+        }
+    }
+
 }
