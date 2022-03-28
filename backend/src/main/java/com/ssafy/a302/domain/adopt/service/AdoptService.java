21ae3b485b6199b87a86f521e5f2b74e41f37a99
김경석 <0xe82de@gmail.com>
2022-03-28 22:12:44 +0900

    #S06P22A302-162 Feat: 입양 인증 요청 서비스 메서드 추가

---
+++ backend/src/main/java/com/ssafy/a302/domain/adopt/service/AdoptService.java
@@ -0,0 +1,8 @@
+package com.ssafy.a302.domain.adopt.service;
+
+import com.ssafy.a302.domain.adopt.service.dto.AdoptDto;
+
+public interface AdoptService {
+
+    Long requestAdoptAuth(Long memberSeq, AdoptDto.AdoptAuth adoptAuth);
+}
