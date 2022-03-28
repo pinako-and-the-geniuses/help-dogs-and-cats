f687b3c4153e998f89e1958e40deb57843949763
안영진 <fufru@naver.com>
2022-03-29 03:21:29 +0900

    #S06P22A302-187 Create: 나이대별 처리상태 Repository 추가

---
+++ a302hadoop/src/main/java/com/ssafy/a302hadoop/domain/hadoop/repository/AgeStateRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.a302hadoop.domain.hadoop.repository;
+
+import com.ssafy.a302hadoop.domain.hadoop.entity.AgeState;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgeStateRepository extends JpaRepository<AgeState, Long> {
+
+
+}
+
+
