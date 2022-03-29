24eef6af75901e3959ad84d3121ef9cf628aa025
안영진 <fufru@naver.com>
2022-03-29 19:31:41 +0900

    #S06P22A302-82 Feat: 유기동물 관련 api Service 코드 추가

--- a302hadoop/src/main/java/com/ssafy/a302hadoop/domain/hadoop/service/HadoopService.java
+++ a302hadoop/src/main/java/com/ssafy/a302hadoop/domain/hadoop/service/HadoopService.java
@@ -1,10 +1,22 @@
 package com.ssafy.a302hadoop.domain.hadoop.service;
 
+import com.ssafy.a302hadoop.domain.hadoop.controller.dto.AnimalDataResponseDto;
 import com.ssafy.a302hadoop.domain.hadoop.service.dto.DataFromHadoopDto;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface HadoopService {
 
     void insertAnimalData(List<DataFromHadoopDto> datasFromHadoopDto, int year, String name);
+
+    Map<Integer, Map<String, Object>> getAnnualBreedData(LocalDate curDate);
+
+    Map<Integer, Map<String, List<AnimalDataResponseDto.AnnualStateInfo>>> getAnnualStateData(LocalDate curDate);
+
+    Map<Integer, Map<String, Map<Integer, List<AnimalDataResponseDto.AgeStateInfo>>>> getAgeStateData(LocalDate curDate);
+
+    Map<Integer, Map<String,Object>> getSpeciesNeutralData(LocalDate curDate);
+
 }
