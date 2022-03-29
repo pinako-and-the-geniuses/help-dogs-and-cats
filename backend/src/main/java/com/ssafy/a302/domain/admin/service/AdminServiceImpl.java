52d825ce19ed24851030ac8669ded04ccf17d05e
limgeonho <ghlim909@gmail.com>
2022-03-30 02:09:35 +0900

    #S06P22A302-169 Feature: 관리자 입양인증 상세페이지 조회

--- backend/src/main/java/com/ssafy/a302/domain/admin/service/AdminServiceImpl.java
+++ backend/src/main/java/com/ssafy/a302/domain/admin/service/AdminServiceImpl.java
@@ -1,11 +1,12 @@
 package com.ssafy.a302.domain.admin.service;
 
 import com.ssafy.a302.domain.admin.service.dto.VolunteerAuthDto;
+import com.ssafy.a302.domain.adopt.entity.AdoptAuth;
+import com.ssafy.a302.domain.adopt.repository.AdoptAuthRepository;
+import com.ssafy.a302.domain.adopt.service.dto.AdoptAuthDto;
 import com.ssafy.a302.domain.volunteer.entity.VolunteerAuth;
 import com.ssafy.a302.domain.volunteer.repository.VolunteerAuthRepository;
-import com.ssafy.a302.domain.volunteer.service.dto.VolunteerDto;
 import com.ssafy.a302.global.constant.ErrorMessage;
-import com.ssafy.a302.global.enums.Status;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ public class AdminServiceImpl implements AdminService{
 
     private final VolunteerAuthRepository volunteerAuthRepository;
 
+    private final AdoptAuthRepository adoptAuthRepository;
 
     @Override
     public VolunteerAuthDto.Response volunteerAuthDetail(Long volunteerSeq) {
@@ -35,9 +37,15 @@ public class AdminServiceImpl implements AdminService{
         VolunteerAuth findVolunteerAuth = volunteerAuthRepository.findByVolunteerSeq(volunteerSeq)
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_VOLUNTEER_AUTH));
 
-
         findVolunteerAuth.changeVolunteerAuthStatus(volunteerAuthDto.getStatus());
+    }
 
+    @Override
+    public AdoptAuthDto.Response adoptAuthDetail(Long adoptSeq) {
 
+        AdoptAuth findAdoptAuth = adoptAuthRepository.findBySeq(adoptSeq)
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_ADOPT_AUTH));
+
+        return findAdoptAuth.toResponseDto();
     }
 }
