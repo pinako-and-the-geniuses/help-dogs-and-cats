ebbc2f95a080177fac33ed1de8cfc641c01db3d9
limgeonho <ghlim909@gmail.com>
2022-03-29 15:07:09 +0900

    #S06P22A302-165 Feature: 관리자 봉사활동 인증 목록 조회

--- backend/src/main/java/com/ssafy/a302/domain/admin/controller/AdminController.java
+++ backend/src/main/java/com/ssafy/a302/domain/admin/controller/AdminController.java
@@ -1,13 +1,21 @@
 package com.ssafy.a302.domain.admin.controller;
 
 
+import com.ssafy.a302.domain.admin.service.AdminService;
+import com.ssafy.a302.domain.volunteer.controller.dto.VolunteerRequestDto;
 import com.ssafy.a302.domain.volunteer.service.VolunteerCommentService;
 import com.ssafy.a302.domain.volunteer.service.VolunteerService;
+import com.ssafy.a302.global.constant.Message;
+import com.ssafy.a302.global.dto.BaseResponseDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,7 +28,19 @@ public class AdminController {
 
     private final VolunteerCommentService volunteerCommentService;
 
+    private final AdminService adminService;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public BaseResponseDto<?> getVolunteerAuthList(Pageable pageable,
+                                                   @RequestParam String order) {
+        //adminService.getPage(pageable, order);
+
+        return BaseResponseDto.builder()
+                .message(Message.SUCCESS_ADMIN_VOLUNTEER_AUTH_LIST)
+                .build();
+    }
 
 
 
