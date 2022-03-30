fdcf3ae57dadace735d7a85920581b972cc0b97f
안영진 <fufru@naver.com>
2022-03-31 00:48:22 +0900

    Fix: hadoop ec2 setting - build shell코드 수정

--- a302hadoop/build_run_all_data_helpdogcat_docker.sh
+++ a302hadoop/build_run_all_data_helpdogcat_docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 아웃풋 디렉토리 삭제
-hdfs dfs -rm -r helpdogcat/output/alldata
+hdfs dfs -rm -r helpdogcatall/output/
 
 # 맵리듀스 실행
-hadoop jar /home/hadoop/a302hadoop/helpdogcatjar/helpdogcat.jar help helpdogcat/input/alldata/alldata.txt helpdogcat/output/alldata
+hadoop jar /home/hadoop/a302hadoop/helpdogcatjar/helpdogcat.jar help helpdogcatall/input/alldata.txt helpdogcatall/output
 
 ## 실행 결과 조회
-#hdfs dfs -cat helpdogcat/output/*
-hdfs dfs -ls -R helpdogcat/output/alldata
+#hdfs dfs -cat helpdogcatall/output/*
+hdfs dfs -ls -R helpdogcatall/output
