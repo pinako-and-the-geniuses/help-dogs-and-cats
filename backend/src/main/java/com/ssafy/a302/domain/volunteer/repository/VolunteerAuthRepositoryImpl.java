c512bc4cc710b6cd86a3503c7eaa52d412fce067
김경석 <0xe82de@gmail.com>
2022-03-30 14:21:57 +0900

    #S06P22A302-165 Feat: 봉사활동 인증 목록 조회용 레파지토리 메서드 추가
    - 카운팅 메서드 추가
    - 인증 데이터 리스트 조회 메서드 추가

--- backend/src/main/java/com/ssafy/a302/domain/volunteer/repository/VolunteerAuthRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/a302/domain/volunteer/repository/VolunteerAuthRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.ssafy.a302.domain.volunteer.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.a302.domain.admin.service.dto.QVolunteerAuthDto_VolunteerAuthPage_VolunteerAuthForPage;
+import com.ssafy.a302.domain.admin.service.dto.VolunteerAuthDto;
 import com.ssafy.a302.domain.volunteer.service.dto.QVolunteerDto_VolunteerAuthDetail_Participant;
 import com.ssafy.a302.domain.volunteer.service.dto.VolunteerDto;
+import com.ssafy.a302.global.enums.Status;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+import static com.ssafy.a302.domain.volunteer.entity.QVolunteerAuth.*;
 import static com.ssafy.a302.domain.volunteer.entity.QVolunteerParticipant.*;
 
 public class VolunteerAuthRepositoryImpl implements VolunteerAuthRepositoryCustom {
@@ -30,6 +36,49 @@ public class VolunteerAuthRepositoryImpl implements VolunteerAuthRepositoryCusto
                 .orderBy(volunteerParticipant.member.seq.asc())
                 .fetch();
 
+        if (list.size() == 0) {
+            list = null;
+        }
+
+        return Optional.ofNullable(list);
+    }
+
+    @Override
+    public Integer countAllByStatus(String search) {
+        JPAQuery<Integer> query = queryFactory
+                .select(volunteerAuth.count().intValue())
+                .from(volunteerAuth)
+                .where(volunteerAuth.volunteer.isDeleted.isFalse());
+
+        if (search.equals("auth")) {
+            query.where(volunteerAuth.status.eq(Status.DONE));
+        } else {
+            query.where(volunteerAuth.status.ne(Status.DONE));
+        }
+
+        return query.fetchOne();
+    }
+
+    @Override
+    public Optional<List<VolunteerAuthDto.VolunteerAuthPage.VolunteerAuthForPage>> findVolunteerAuthForPageDto(Pageable pageable, String search) {
+        JPAQuery<VolunteerAuthDto.VolunteerAuthPage.VolunteerAuthForPage> query = queryFactory
+                .select(new QVolunteerAuthDto_VolunteerAuthPage_VolunteerAuthForPage(
+                        volunteerAuth.seq,
+                        volunteerAuth.volunteer.title,
+                        volunteerAuth.status))
+                .from(volunteerAuth)
+                .where(volunteerAuth.volunteer.isDeleted.isFalse())
+                .offset((long) (pageable.getPageNumber() - 1) * pageable.getPageSize())
+                .limit(pageable.getPageSize())
+                .orderBy(volunteerAuth.seq.desc());
+
+        if (search.equals("auth")) {
+            query.where(volunteerAuth.status.eq(Status.DONE));
+        } else {
+            query.where(volunteerAuth.status.ne(Status.DONE));
+        }
+
+        List<VolunteerAuthDto.VolunteerAuthPage.VolunteerAuthForPage> list = query.fetch();
         if (list.size() == 0) {
             list = null;
         }
