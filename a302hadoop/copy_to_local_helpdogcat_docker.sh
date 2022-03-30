e14992164f32e2e0ec5af686b642dcce3d5c126c
안영진 <fufru@naver.com>
2022-03-31 00:48:34 +0900

    Fix: hadoop ec2 setting - local copy shell코드 수정

--- a302hadoop/copy_to_local_helpdogcat_docker.sh
+++ a302hadoop/copy_to_local_helpdogcat_docker.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 # hdfs에 있는 아웃풋 데이터 로컬로 복사
-hdfs dfs -get -f helpdogcat/output/recentdata/part-r-00000 /home/hadoop/a302hadoop/hadoopoutput/recentdata
-hdfs dfs -get -f helpdogcat/output/recentdata/part-r-00001 /home/hadoop/a302hadoop/hadoopoutput/recentdata
-hdfs dfs -get -f helpdogcat/output/recentdata/part-r-00002 /home/hadoop/a302hadoop/hadoopoutput/recentdata
-hdfs dfs -get -f helpdogcat/output/recentdata/part-r-00003 /home/hadoop/a302hadoop/hadoopoutput/recentdata
+
+rm -rf /home/hadoop/a302hadoop/hadoopoutput/recentdata
+mkdir /home/hadoop/a302hadoop/hadoopoutput/recentdata
+
+hdfs dfs -get -f helpdogcat/output/part-r-00000 /home/hadoop/a302hadoop/hadoopoutput/recentdata
+hdfs dfs -get -f helpdogcat/output/part-r-00001 /home/hadoop/a302hadoop/hadoopoutput/recentdata
+hdfs dfs -get -f helpdogcat/output/part-r-00002 /home/hadoop/a302hadoop/hadoopoutput/recentdata
+hdfs dfs -get -f helpdogcat/output/part-r-00003 /home/hadoop/a302hadoop/hadoopoutput/recentdata
 
 
 ls -al "/home/hadoop/a302hadoop/hadoopoutput/recentdata"
