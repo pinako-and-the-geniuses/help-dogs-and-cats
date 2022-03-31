1571289c932f2ada9206b3c570b3f98af18ff19e
limgeonho <ghlim909@gmail.com>
2022-03-31 14:50:49 +0900

    #S06P22A302-196 :Fix - 봉사활동 댓글 존재하는 것만 조회

--- backend/src/main/java/com/ssafy/a302/domain/volunteer/repository/VolunteerCommentRepositoryCustom.java
+++ backend/src/main/java/com/ssafy/a302/domain/volunteer/repository/VolunteerCommentRepositoryCustom.java
@@ -1,10 +1,13 @@
 package com.ssafy.a302.domain.volunteer.repository;
 
 import com.ssafy.a302.domain.volunteer.entity.VolunteerComment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface VolunteerCommentRepositoryCustom {
-    Optional<List<VolunteerComment>> findCommentsByVolunteerSeq(Long volunteerSeq);
+    @Query("SELECT vc FROM VolunteerComment vc WHERE vc.volunteer.seq = :seq AND vc.isDeleted = FALSE")
+    Optional<List<VolunteerComment>> findCommentsByVolunteerSeq(@Param(value = "seq") Long seq);
 }
