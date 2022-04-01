01a9ddd835bd72552fd4e498d95c2c05c17f59ec
jiho <hahhaah@naver.com>
2022-04-02 02:16:16 +0900

    #S06P22A302-52 Fix: 댓글 코드 정리

--- frontend/src/components/Comment/Comment.js
+++ frontend/src/components/Comment/Comment.js
@@ -57,40 +57,27 @@ function Comment(props){
                 (props.comments).map((comment)=>{
                     return(
                         <div key={comment.commentSeq}>
-                        <div className={style.comment}>
-                            {/* 이미지 수정 필요 */}
-                            <img src={comment.writerProfileImagePath}/>
-                            <div className={style.content}>
-                                <div className={style.top}>
-                                    <div className={style.left}>
-                                        <span className={style.writer}>{comment.writerNickname}</span>
-                                        <span className={style.date}>{comment.createdDate}</span>
+                            <div className={style.comment}>
+                                {/* 이미지 수정 필요 */}
+                                <img src={comment.writerProfileImagePath}/>
+                                <div className={style.content}>
+                                    <div className={style.top}>
+                                        <div className={style.left}>
+                                            <span className={style.writer}>{comment.writerNickname}</span>
+                                            <span className={style.date}>{comment.createdDate}</span>
+                                        </div>
+                                        {
+                                            memSeq === comment.writerSeq
+                                            ? <p className={style.delete} onClick={()=>{deleteHandler(comment.commentSeq)}}>삭제</p>
+                                            : null
+                                        }
                                     </div>
-                                    {
-                                        memSeq === comment.writerSeq
-                                        ? <p className={style.delete} onClick={()=>{deleteHandler(comment.commentSeq)}}>삭제</p>
-                                        : null
-                                    }
+                                    <p onClick={openReply}>
+                                        {comment.content}
+                                    </p>
                                 </div>
-                                <p onClick={openReply}>
-                                    {comment.content}
-                                </p>
                             </div>
-                        </div>
-                        {/* <Reply />
-                        {
-                            rereply
-                            ?(
-                                <div className={style.addReply}>
-                                    <div></div>
-                                    <textarea type="text"/>
-                                    <button type="submit" className={style.replyBtn}>등록</button>
-                                    <button className={style.XBtn} onClick={closeReply}>X</button>
-                                </div>
-                            )
-                            : ""
-                        } */}
-                        <hr />
+                            <hr />
                         </div>
                     )
                 })
