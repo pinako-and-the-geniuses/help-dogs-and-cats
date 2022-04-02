7675c71c988431e837d6be3909baaaac7a6dfb91
jiho <hahhaah@naver.com>
2022-04-03 02:57:01 +0900

    #S06P22A302-92 Fix: 지역 데이터 분리

---
+++ frontend/src/pages/volunteer/areaData.js
@@ -0,0 +1,21 @@
+/*eslint-disable*/
+export default [
+    {value: '전체', name: '전체'},
+    {value: '서울', name: '서울'},
+    {value: '부산', name: '부산'},
+    {value: '대구', name: '대구'},
+    {value: '인천', name: '인천'},
+    {value: '광주', name: '광주'},
+    {value: '세종', name: '세종'},
+    {value: '대전', name: '대전'},
+    {value: '울산', name: '울산'},
+    {value: '경기', name: '경기'},
+    {value: '강원', name: '강원'},
+    {value: '충북', name: '충북'},
+    {value: '충남', name: '충남'},
+    {value: '전북', name: '전북'},
+    {value: '전남', name: '전남'},
+    {value: '경북', name: '경북'},
+    {value: '경남', name: '경남'},
+    {value: '제주', name: '제주'},
+]
