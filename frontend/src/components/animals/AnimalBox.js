7ef33f3f8ddfa5a8638c65f4466393ecff9a1915
jiho <hahhaah@naver.com>
2022-04-04 02:20:21 +0900

    #S06P22A302-4 Design: 동물 카드 수정

--- frontend/src/components/animals/AnimalBox.js
+++ frontend/src/components/animals/AnimalBox.js
@@ -28,12 +28,12 @@ export default function AnimalBox(props) {
         const data = item.children;
         return (
           <div className="col" key={data[0].value}>
-            <div className={cn(`${st.card}`, "card h-100")}>
+            <div className={cn(`${st.card}`)}>
               <img
                 src={data[11].value}
                 className={st.cardImgTop}
                 alt="사진없음"/>
-              <div className="card-body">
+              <div className={st.cardBody}>
                 <div className={st.processState}>{data[12].value}</div>
                 <div className={cn(`${st.cardText}`, "card-text")}>
                   <span className={st.title}>공고번호</span>
@@ -92,67 +92,6 @@ export default function AnimalBox(props) {
   return (
     <>
       {onReturn()}
-      {/* {list ? (
-        list.map((item, index) => {
-          const data = item.children;
-          return (
-            <div className="col" key={data[0].value}>
-              <div className={cn(`${st.card}`, "card h-100")}>
-                <img
-                  src={data[1].value}
-                  className={st.cardImgTop}
-                  alt="사진없음"
-                  onClick={() => onGoDetail(index)}
-                />
-
-                <div className="card-body">
-                  <div className={cn(`${st.cardText}`, "card-text")}>
-                    <span className={st.title}>공고번호</span>
-                    <span>{data[0].value}</span>
-                  </div>
-                  <div className={cn(`${st.cardText}`, "card-text")}>
-                    <span className={st.title}>접수일시</span>
-                    <span>
-                      {data[2].value.slice(0, 4)}-{data[2].value.slice(4, 6)}-
-                      {data[2].value.slice(6)}
-                    </span>
-                  </div>
-                  <div className={cn(`${st.cardText}`, "card-text")}>
-                    <span className={st.title}>관할기관</span>
-                    <p className="mt-1 ms-5">{data[16].value}</p>
-                  </div>
-                  <div className={cn(`${st.cardText}`, "card-text")}>
-                    <div>
-                      <span className={st.title}>발견장소</span>
-                    </div>
-                    <div>
-                      <p>{data[3].value}</p>
-                    </div>
-                  </div>
-                  <div className={st.detilaBtn}>
-                    <button
-                      // href={`/animals/detail/${data[0].value}`}
-                      className="btn btn-primary"
-                    >
-                      자세히 보기
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })
-      ) : (
-        <>
-          <div></div>
-          <div> 데이터가 없습니다. </div>
-          <div>
-            <h2>
-              <img src={loading} alt="로딩중" />
-            </h2>
-          </div>
-        </>
-      )} */}
     </>
   );
 }
