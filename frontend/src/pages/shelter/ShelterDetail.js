21ea1cc1adfbb05e2f16e923b5030c50e7cae09e
hyojeong721 <ase0574@naver.com>
2022-04-05 14:08:50 +0900

    #S06P22A302-176 Fix: 조회페이지 레이아웃 통일

--- frontend/src/pages/shelter/ShelterDetail.js
+++ frontend/src/pages/shelter/ShelterDetail.js
@@ -15,50 +15,51 @@ function ShelterDetail() {
 
   return (
     <div className={style.shelter_container}>
-      <header>
-        <h2>동물 보호 센터</h2>
-      </header>
+      <main className={style.detailmain}>
+        <header>
+          <h2>동물 보호 센터</h2>
+        </header>
+        <section className={style.topContent}>
+          <div className={style.row}>
+            <div className={cn(style.main_info, "col-6")}>
+              <div className={style.text_box}>
+                <p className={style.title}>보호센터명</p>
+                <p>{data.careNm}</p>
+              </div>
+              <div className={style.text_box}>
+                <p className={style.title}>전화번호</p>
+                <p>{data.careTel}</p>
+              </div>
+              <div className={style.text_box}>
+                <p className={style.title}>주소</p>
+                <p>{data.careAddr}</p>
+              </div>
+              <div className={style.text_box}>
+                <p className={style.title}>관리기관명</p>
+                <p>{data.careAddr}</p>
+              </div>
+              <div className={style.text_box}>
+                <p className={style.title}>운영시간</p>
+                <p>
+                  {data.weekOprStime} ~ {data.weekOprEtime}
+                </p>
+              </div>
+              <div className={style.text_box}>
+                <p className={style.title}>유형</p>
+                <p>{data.divisionNm}</p>
+              </div>
+              <div className={style.text_box}>
+                <p className={style.title}>구조대상동물</p>
+                <p>{data.saveTrgtAnimal}</p>
+              </div>
+            </div>
 
-      <section className={cn("container")}>
-        <div className={cn("row")}>
-          <div className={cn(style.main_info, "col-6")}>
-            <div className={style.text_box}>
-              <p className={style.title}>보호센터명</p>
-              <p>{data.careNm}</p>
-            </div>
-            <div className={style.text_box}>
-              <p className={style.title}>전화번호</p>
-              <p>{data.careTel}</p>
-            </div>
-            <div className={style.text_box}>
-              <p className={style.title}>주소</p>
-              <p>{data.careAddr}</p>
-            </div>
-            <div className={style.text_box}>
-              <p className={style.title}>관리기관명</p>
-              <p>{data.careAddr}</p>
-            </div>
-            <div className={style.text_box}>
-              <p className={style.title}>운영시간</p>
-              <p>
-                {data.weekOprStime} ~ {data.weekOprEtime}
-              </p>
-            </div>
-            <div className={style.text_box}>
-              <p className={style.title}>유형</p>
-              <p>{data.divisionNm}</p>
-            </div>
-            <div className={style.text_box}>
-              <p className={style.title}>구조대상동물</p>
-              <p>{data.saveTrgtAnimal}</p>
+            <div className={cn(style.map, "col-6")}>
+              {data.lat && data.lng && <Map lat={data.lat} lng={data.lng} />}
             </div>
           </div>
-
-          <div className={cn(style.map, "col-6")}>
-            {data.lat && data.lng && <Map lat={data.lat} lng={data.lng} />}
-          </div>
-        </div>
-      </section>
+        </section>
+      </main>
     </div>
   );
 }
