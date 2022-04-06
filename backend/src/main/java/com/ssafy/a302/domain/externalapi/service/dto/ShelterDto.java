66e1f35b218ac9d44806831fb7e1e2e96e50472e
김경석 <0xe82de@gmail.com>
2022-04-07 02:44:16 +0900

    #S06P22A302-227 Create: 보호소 DTO 생성

---
+++ backend/src/main/java/com/ssafy/a302/domain/externalapi/service/dto/ShelterDto.java
@@ -0,0 +1,44 @@
+package com.ssafy.a302.domain.externalapi.service.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(of = {"shelterName", "organizationName", "divisionName", "saveTargetAnimal", "address", "jibunAddress", "closeDay", "vetPersonCount", "specsPersonCount", "tel"})
+public class ShelterDto {
+
+    private final String shelterName;
+
+    private final String organizationName;
+
+    private final String divisionName;
+
+    private final String saveTargetAnimal;
+
+    private final String address;
+
+    private final String jibunAddress;
+
+    private final String closeDay;
+
+    private final Integer vetPersonCount;
+
+    private final Integer specsPersonCount;
+
+    private final String tel;
+
+    @Builder
+    public ShelterDto(String shelterName, String organizationName, String divisionName, String saveTargetAnimal, String address, String jibunAddress, String closeDay, Integer vetPersonCount, Integer specsPersonCount, String tel) {
+        this.shelterName = shelterName;
+        this.organizationName = organizationName;
+        this.divisionName = divisionName;
+        this.saveTargetAnimal = saveTargetAnimal;
+        this.address = address;
+        this.jibunAddress = jibunAddress;
+        this.closeDay = closeDay;
+        this.vetPersonCount = vetPersonCount;
+        this.specsPersonCount = specsPersonCount;
+        this.tel = tel;
+    }
+}
